af82d6c7fdff0e7b6ae7dbef74932a16bb564057
= <=>
2018-05-05 21:16:56 +0800

    添加sql文件

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 基于Spring MVC的demo，包含几篇介绍web开发的博客
 
 [第一篇博客地址](https://github.com/XingToMax/DesignPatternDemo/tree/master/blogs)
+
++ blogs目录下为博客，点开目录的README.md即为博客目录
++ sql目录下放了项目中出现表的sql文件
++ [演示地址](https://tomax.xin/dpd)
---
+++ sql/dpd.sql
@@ -0,0 +1,55 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.6.15 : Database - design_pattern
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`design_pattern` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `design_pattern`;
+
+/*Table structure for table `hello` */
+
+DROP TABLE IF EXISTS `hello`;
+
+CREATE TABLE `hello` (
+  `name` varchar(32) DEFAULT NULL,
+  `signature` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `message` */
+
+DROP TABLE IF EXISTS `message`;
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `content` varchar(128) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
